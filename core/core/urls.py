6da16058358f87080fd0d6e7c1b4fabea20dc536
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 14:36:05 +0330

    add urls file to wiki app

--- core/core/urls.py
+++ core/core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('wiki', include('wiki.urls')),
+    path('wiki/', include('wiki.urls')),
 
 ]
