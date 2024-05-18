b4c507939e94efa7ae3111313c8c60bc9538459c
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 15:32:08 +0330

    add article detail view

--- core/wiki/urls.py
+++ core/wiki/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView,ArticleDetailView
 app_name = 'wiki'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name = 'index')
+    path('', IndexView.as_view(), name = 'index'),
+    path('detail/<int:pk>/', ArticleDetailView.as_view(), name='detail')
 ]
--- core/wiki/views.py
+++ core/wiki/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from .models import Article
-
+from django.views.generic import DetailView
 # Create your views here.
 class IndexView(TemplateView):
     template_name= 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.filter(is_published = True)
+        return context
+    
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name =  'detail.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
