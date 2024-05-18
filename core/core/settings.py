cb16400f59e7f4a4397110af1883ea322015a41e
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 16:49:01 +0330

    add article image

--- core/core/settings.py
+++ core/core/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -118,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+if not DEBUG:
+    STATIC_ROOT = "/home/django/www-data/example.com/static/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/wiki/models.py
+++ core/wiki/models.py
@@ -9,7 +9,11 @@ class Article(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    image = models.ManyToManyField('Image', blank=True)
+    image1 = models.ImageField(upload_to='images/', blank=True)
+    image2 = models.ImageField(upload_to='images/', blank=True)
+    image3 = models.ImageField(upload_to='images/', blank=True)
+    image4 = models.ImageField(upload_to='images/', blank=True)
+
 
     def __str__(self):
         return self.title
@@ -18,14 +22,6 @@ class Article(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
-
-    def __str__(self):
-        return self.name
-
-class Image(models.Model):
-    name = models.CharField(max_length=200)
-    file = models.FileField(upload_to='images/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
