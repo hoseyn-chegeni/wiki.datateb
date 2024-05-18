f43b9f02f29142deb1b02e126660acf7ded47fea
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 15:08:53 +0330

    add image , article and category model

--- core/wiki/models.py
+++ core/wiki/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    author = models.ForeignKey('accounts.User', on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ManyToManyField('Image', blank=True)
+
+    def __str__(self):
+        return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=200)
+    file = models.FileField(upload_to='images/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
