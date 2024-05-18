4831ece16f39ff93111d6ae89411d5b351e399e7
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-18 15:04:25 +0330

    add user model and migrate db

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(max_length=255, blank=True, null=True)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    image = models.ImageField(
+        upload_to="images", default="images/default.png", blank=True, null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        "User", on_delete=models.SET_NULL, blank=True, null=True
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}"
