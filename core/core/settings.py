e7cd1d740d1d61e16ba603a2c8438e77ffe08e46
hoseyn <87471826+hoseynchegeni@users.noreply.github.com>
2024-05-24 22:09:29 +0330

    prepare for deploy

--- core/core/settings.py
+++ core/core/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
